0bd9f6cfda6a0293e2775441abc2dcc756a69c10
Daniel Lozano <d.lozanonavas@outlook.com>
2021-02-23 13:32:36 -0500

    Adds: Initial Files

---
+++ Data/InstallDto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FenixAlliance.ABS.Installer.Data
+{
+    public  class InstallDto
+    {
+        [Required]
+        public int DBPort { get; set; } = 3306;
+        [Required]
+        public string DBHost { get; set; } = "localhost";
+        [Required]
+        public string DBName { get; set; }
+        [Required]
+        public string DBEngine { get; set; } = "MySQL";
+        [Required]
+        public string DBUserName { get; set; }
+        [Required]
+        public string DBUserSecret { get; set; }
+        [Required]
+        public string TenantName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string TenantEmail { get; set; }
+        [Required]
+        public string TenantCountry { get; set; } = "USA";
+        [Required]
+        public string TenantCurrency { get; set; } = "USD";
+        [Required]
+        public string PortalTitle { get; set; }
+        [Required]
+        public string PortalDescription { get; set; }
+        [Required]
+        public bool PortalVisibility { get; set; }
+        [Required]
+        public string HolderName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string HolderEmail { get; set; }
+        [Required]
+        public string HolderPassword { get; set; }
+        [Required]
+        public string HolderCurrency { get; set; } = "USD";
+    }
+}
